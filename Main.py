f23cefa6c4be332c2690241e3d4f7b40cf1f323c
Splixxy <71837246+00318345@users.noreply.github.com>
2021-04-10 12:12:13 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,6 +1,6 @@
 #imports contab package
 from crontab import CronTab
-#creates the cron job of Detect-IP.py script to run every hour
+#creates the cron job of Users.py script to run every hour
 cron = CronTab(user="root")
 Usersjob = cron.new(command="python3 Users.py")
 Usersjob.hour.every(1)
